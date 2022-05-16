69c715fe35543748537c363eed7658acdb43af88
William Peirs <wpeirs@gmail.com>
2022-05-16 15:16:26 +0200

    coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:ask]
+    if @question == "I am going to work"
+      @response = "Great !"
+    elsif @question.end_with?("?")
+      @response = "Silly question, get dressed and go to work!"
+    elsif @response == "I don't care,get dressed and go to work !"
+    end
+  end
+end
